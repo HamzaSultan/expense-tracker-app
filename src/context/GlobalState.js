52cd5d5ee71f489301fcbc593f7abd90f28eb0c9
HamzaSultan <56472325+HamzaSultan@users.noreply.github.com>
2020-06-22 23:40:34 +0500

    initial commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,51 @@
+
+
+import React, { createContext, useReducer } from 'react';
+
+// Importing the Reducer
+import AppReducer from './AppReducer';
+
+// Creating the initial state
+const initialState = {
+    transactions: []
+}
+
+// Creating the Global Context
+export const GlobalContext = createContext(initialState);
+
+// Creating a Provider for the Global Context
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions for Transactions
+
+        // Delete Existing Transaction Action
+        function delTransaction(id) {
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            });
+        }
+
+        // Add New Transaction Action
+        function addTransaction(transaction) {
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: transaction
+            })
+        }
+
+    return (
+        <GlobalContext.Provider value={
+            {
+                transactions: state.transactions,
+                delTransaction,
+                addTransaction
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    );
+
+}
+
